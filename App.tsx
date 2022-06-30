9810c0b03602d459a5de6fd0adb566df72f33fe0
周飞 <zhoufei@gaosiedu.com>
2022-07-01 07:45:09 +0800

    feat: 添加pretier&git&husky规范

--- App.tsx
+++ App.tsx
@@ -10,61 +10,58 @@
 
 import React, { useEffect } from 'react';
 import {
-    SafeAreaView,
-    ScrollView,
-    StatusBar,
-    StyleSheet,
-    Text,
-    useColorScheme,
-    View,
-    Dimensions,
-    Alert
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+  Dimensions,
+  Alert,
 } from 'react-native';
 
-import {
-    Colors,
-    Header,
-} from 'react-native/Libraries/NewAppScreen';
+import { Colors, Header } from 'react-native/Libraries/NewAppScreen';
 
 const App = () => {
-    const isDarkMode = useColorScheme() === 'dark';
+  const isDarkMode = useColorScheme() === 'dark';
 
-    const backgroundStyle = {
-        backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-        height: Dimensions.get('window').height
-    };
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    height: Dimensions.get('window').height,
+  };
 
-    return (
-        <SafeAreaView style={ backgroundStyle }>
-            <StatusBar barStyle={ isDarkMode ? 'light-content' : 'dark-content' }/>
-            <ScrollView
-                contentInsetAdjustmentBehavior="automatic"
-                style={ backgroundStyle }>
-                <Header/>
-                <Text>欢迎来到周飞的APP</Text>
-                <Text>欢迎来到周飞的APP</Text>
-            </ScrollView>
-        </SafeAreaView>
-    );
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+        <Header />
+        <Text>欢迎来到周飞的APP</Text>
+        <Text>欢迎来到周飞的APP</Text>
+      </ScrollView>
+    </SafeAreaView>
+  );
 };
 
 const styles = StyleSheet.create({
-    sectionContainer: {
-        marginTop: 32,
-        paddingHorizontal: 24
-    },
-    sectionTitle: {
-        fontSize: 24,
-        fontWeight: '600'
-    },
-    sectionDescription: {
-        marginTop: 8,
-        fontSize: 18,
-        fontWeight: '400'
-    },
-    highlight: {
-        fontWeight: '700'
-    }
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
 });
 
 export default App;
